ad8a58004d6a74067c4e3b6c2236da5ec89f41dd
Samuel Parra <espaitk@gmail.com>
2022-10-14 07:10:30 -0400

    Habladores

---
+++ js/variables.js
@@ -0,0 +1,20 @@
+
+
+
+
+const descripcion = [
+    {
+        codigo: '',
+        descripcion: '',
+        precio: '',
+    }
+]
+
+const placetoAdd = document.querySelector('.insert');
+
+/* <div class="base ">
+            <h2 class="code">${codigo}</h2>
+            <p class="descripcion">${descripcion}</p>
+            <h3 class="descripcion">${precio}</h3>
+        </div>
+ */
