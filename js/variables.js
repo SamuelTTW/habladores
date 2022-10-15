83f89d3c6ee5e6ad45aad4aecaff62e635bf6dee
Samuel Parra <espaitk@gmail.com>
2022-10-15 05:17:39 -0400

    Hablador tiras

--- js/variables.js
+++ js/variables.js
@@ -1,20 +1,176 @@
 
+        const codigo = 'SKU: 2202034';
+        const descripcion = 'BOLSA CLIENTE DORITOS CHESMINANIA VENTO AUREO SERGIOLOL';
+        const precio = '2,40';
 
-
-
-const descripcion = [
-    {
-        codigo: '',
-        descripcion: '',
-        precio: '',
-    }
-]
 
 const placetoAdd = document.querySelector('.insert');
 
-/* <div class="base ">
+
+    placetoAdd.insertAdjacentHTML('afterbegin',`
+
+    <div class="base">
+            <div class="separador">
             <h2 class="code">${codigo}</h2>
             <p class="descripcion">${descripcion}</p>
-            <h3 class="descripcion">${precio}</h3>
         </div>
- */
+            <div class="gota">
+            <h3 class="precio">${precio}</h3>
+            </div>
+        </div>
+
+        <div class="base">
+        <div class="separador">
+        <h2 class="code">${codigo}</h2>
+        <p class="descripcion">${descripcion}</p>
+    </div>
+        <div class="gota">
+        <h3 class="precio">${precio}</h3>
+        </div>
+    </div>
+    <div class="base">
+    <div class="separador">
+    <h2 class="code">${codigo}</h2>
+    <p class="descripcion">${descripcion}</p>
+</div>
+    <div class="gota">
+    <h3 class="precio">${precio}</h3>
+    </div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+<div class="base">
+<div class="separador">
+<h2 class="code">${codigo}</h2>
+<p class="descripcion">${descripcion}</p>
+</div>
+<div class="gota">
+<h3 class="precio">${precio}</h3>
+</div>
+</div>
+
+
+
+`);
+
+
+
+
